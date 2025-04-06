4213019737f413991b903d512db66ed556510652
MartinLion2GDA <martin.lion04@gmail.com>
2025-04-06 17:09:01 +0200

    Ajout de sprites sheets + trigger et bool d'animations
    + assets artistiques apparement

---
+++ Assets/Scripts/Martin/Animation_player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_player : MonoBehaviour
+{
+    private Animator _animator;
+
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _animator.SetTrigger("tempWalk");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _animator.SetTrigger("tempWalk");
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _animator.SetTrigger("tempWalk");
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _animator.SetTrigger("tempWalk");
+
+        }
+    }
+}
